cf71fbac95b322b6d15032bc36fc21cb8b611d81
wwwls99 <201810806@kyonggi.ac.kr>
2022-05-13 16:46:46 +0900

    [남진수] 리액트 2회차 실습

--- src/views/user/WwwlsView.js
+++ src/views/user/WwwlsView.js
@@ -2,13 +2,53 @@ import React from 'react';
 
 const element = <h1>Hello world! 남진수 hihi</h1>;
 
+const Hello = (props) => {
+  return <h1>HiHI, {props.name} {props.age}</h1>
+}
+
+const HelloGroup = (props) => {
+  return (
+    <>
+      <Hello name="남진수"/>
+      <Hello age="50" name="ska"/>
+      <Hello name="남"/>
+    </>
+  )
+}
+
 class WwwlsView extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {date: new Date()};
+  }
+  
+  componentDidMount(){
+    setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+  tick(){
+    this.setState({
+      date: new Date()
+    })
+  }
+
   render() {
+    
+    const Time = () => {
+      return(
+        <h1>지금 시각은 {this.state.date.toLocaleTimeString()}</h1>
+      )
+    }  
+
     return (
       <>
         <div>
           {element}
         </div>
+        <HelloGroup/>
+        <Time/>
       </>
     );  
   }
