8199eac2d40230983178a245b17ce3358eb62cc7
wwwls99 <201810806@kyonggi.ac.kr>
2022-05-13 14:38:57 +0900

    test

--- src/views/user/WwwlsView.js
+++ src/views/user/WwwlsView.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const element = <h1>Hello world! 남진수 hi</h1>;
+const element = <h1>Hello world! 남진수 hihi</h1>;
 
 class WwwlsView extends React.Component {
   render() {
